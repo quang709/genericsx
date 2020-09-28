386d7de57480e44fa8774642fd5fec60770943f9
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-28 12:34:02 +0700

    generics

---
+++ MyGeneric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace generics
+{
+    public class MyGeneric<T>
+    {
+        public  T a;
+        public   T b;
+        public void SoSanh<T>(T a, T b)
+        {
+            if (a.Equals(b))
+            {
+
+                Console.WriteLine($"{a} bang {b}");
+            }
+            else
+            {
+
+                Console.WriteLine($"{a} khong bang {b}");
+            }
+        }
+    }
+}   
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGeneric<int> myGeneric = new MyGeneric<int>();
+            myGeneric.SoSanh<int>(2, 2);
+            myGeneric.SoSanh<string>("abc", "abc");
+
+        }
+    }
+}
